0673784bffcd8578b96cbf37cecd57b2b8a15669
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-07-20 19:20:31 +0200

    Delete the comments in MessageList.js

--- client/MessageList.js
+++ client/MessageList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './MessageList.css';
 
-//komponent prezentacyjny
 const Message = props => (
   <div className={styles.Message}>
     <strong>{props.from} :</strong>
@@ -12,9 +11,7 @@ const Message = props => (
 const MessageList = props => (
   <div className={styles.MessageList}>
     {
-      //mapujemy po liscie wiadomosci, korzysta ze stworzonego
-      //wyzej komponentu meddage
-      props.messages.map((message, i) => { //skad i czerpie???
+      props.messages.map((message, i) => {
         return (
           <Message
             key={i}
