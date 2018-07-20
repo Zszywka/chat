4a78505f7cdd0c2472594782b4304ceb14d341b2
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-07-20 19:21:18 +0200

    Delete the comments in UserForm.js

--- client/UserForm.js
+++ client/UserForm.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import styles from './UserForm.css';
-//form for your nick
-//komponenty-kontenery(posiadają swój wewnętrzny stan)-class
+
 class UserForm extends Component {
   constructor(props) {
     super(props);
@@ -9,20 +8,14 @@ class UserForm extends Component {
   }
 
   handleSubmit(e) {
-    //zatwierdza formularz(gdzie poczatkowa wartosc to puste pole)
-    //zapobiec zachowaniu formularza(przesyłaniu formularza przez przycisk, lub
-    //podążaniu za adresem URL)
     e.preventDefault();
-    //modyfikuje stan w komponencie App
-    //do met onUserSubmit przekazuje sie to co wpisano w formularz
-    this.props.onUserSubmit(this.state.name); //????Sakd metoda onUserSubmit???
+    this.props.onUserSubmit(this.state.name);
   }
-  //odbiera wiadomosc jaka wpisalismy w input(nazwa uzytkownika)
+
   handleChange(e) {
-    //modyfikuje stan unputa, zmienia text w inpucie
     this.setState({ name : e.target.value });
   }
-  //do wpisania nazwy uzytkownika
+
   render() {
     return(
       <form className={styles.UserForm} onSubmit={e => this.handleSubmit(e)}>
